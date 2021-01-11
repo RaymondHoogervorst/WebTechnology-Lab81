92381eff9ee5c9be3cfe16e2967f6b911f5ac66a
RaymondHoogervorst <raymond.hoogervorst4@gmail.com>
2021-01-11 14:27:24 +0100

    Fixed error with sort input and added sortTable function

--- script.js
+++ script.js
@@ -8,23 +8,46 @@ function resetDatabase() {
 }
 
 function changeSortPreference(event) {
-
    //Detecting column to sort
    let prevCol = sortCol;
-   sortCol = event.target.className.charAt(3) - 1;
+   sortCol = this.className.charAt(3) - 1;
 
    //Checking if column is already being sorted
    if (prevCol === sortCol) {
+      //If the column is already being sorted, flip the direction
       sortAscending = !sortAscending;
    }
    else {
+      //otherwise, initially sort ascending
       sortAscending = true;
       if (prevCol >= 0) {
-         console.log(prevCol);
-         event.target.parentElement.children[prevCol].children[0].innerHTML = "&#9644";
+         this.parentElement.children[prevCol].children[0].innerHTML = "&#9644";
       }
    };
-   event.target.children[0].innerHTML = sortAscending ? "&#9650" : "&#9660";
+   this.children[0].innerHTML = sortAscending ? "&#9650" : "&#9660";
+
+   sortTable();
+}
+
+function sortTable() {
+   //Extracting table rows
+   $datarows = $(".odd, .even").detach();
+
+   //Sorting according to preferences
+   $datarows = $datarows.sort(function(a, b) {
+      if ($(a).children().get(sortCol).innerHTML > $(b).children().get(sortCol).innerHTML ^ sortAscending) {
+         return 1;
+      }
+      else {
+         return -1;
+      }
+   });
+
+   //Assigning odd/even classes and placing rows back
+   $datarows.each(function(index, value) {
+      value.className = (index % 2 === 0) ? "odd" : "even";
+      $(".tableheader").after(value);
+   });
 }
 
 function main() {
