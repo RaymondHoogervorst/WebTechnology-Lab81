1e6bb890df253b2c721b303d48e9d04bf6737092
RaymondHoogervorst <raymond.hoogervorst4@gmail.com>
2021-01-11 12:20:58 +0100

    Created sorting input

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 $(main);
 
+let sortCol = -1;
+let sortAscending = true;
+
 function resetDatabase() {
    $.get("https://wt.ops.labs.vu.nl/api21/0a262ecd/reset");
+}
+
+function changeSortPreference(event) {
+
+   //Detecting column to sort
+   let prevCol = sortCol;
+   sortCol = event.target.className.charAt(3) - 1;
+
+   //Checking if column is already being sorted
+   if (prevCol === sortCol) {
+      sortAscending = !sortAscending;
+   }
+   else {
+      sortAscending = true;
+      if (prevCol >= 0) {
+         console.log(prevCol);
+         event.target.parentElement.children[prevCol].children[0].innerHTML = "&#9644";
+      }
+   };
+   event.target.children[0].innerHTML = sortAscending ? "&#9650" : "&#9660";
 }
 
 function main() {
    //Reset button detection
    $("#resetButton").click(resetDatabase);
+
+   //Changing sort preferences detection
+   $(".tableheader span").parent().click(changeSortPreference);
 }
