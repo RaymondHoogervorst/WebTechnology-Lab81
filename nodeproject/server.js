63f6e8807e5819d87ce178464813adb44aa6c26c
RaymondHoogervorst <raymond.hoogervorst4@gmail.com>
2021-01-19 13:35:05 +0100

    Redid basics of server.js

--- nodeproject/server.js
+++ nodeproject/server.js
@@ -1,5 +1,11 @@
+const sqlite = require('sqlite3').verbose();
+let db = my_database('./products.db');
+
 const express = require("express");
 const app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
