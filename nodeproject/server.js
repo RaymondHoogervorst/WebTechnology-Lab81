9d8bee37237eef15c66e9f776f10cab3df47df86
RaymondHoogervorst <raymond.hoogervorst4@gmail.com>
2021-01-22 13:59:26 +0100

    Added better error handling and a few comments

--- nodeproject/server.js
+++ nodeproject/server.js
@@ -33,30 +33,37 @@ const express = require("express");
 const app = express();
 
 var bodyParser = require("body-parser");
-const { request } = require('express');
 app.use(bodyParser.json());
 
+
+//RETRIEVE ALL
 app.get('/', (req, res) => {
-   db.all('SELECT * FROM products', function(err, result) {
-      sendRequest(req, res, result);
+   db.all('SELECT * FROM products', function(err, result) {  
+      if (err) {sendRequest(req, res, err, 500)}
+      else {
+         sendRequest(req, res, result);
+      }
    })
 });
 
+//RETRIEVE
 app.get('/:productID', (req, res) => {
-   db.serialize (() => {
       db.all('SELECT * FROM products WHERE id = ' + req.params.productID, function(err, result) {
-         if(result.length === 1) {
+         if (err) {sendRequest(req, res, err, 500)}
+         else if(result.length === 1) {
             sendRequest(req, res, result[0]);
          }
          else {
             sendRequest(req, res, "No item in our database has ID: " + req.params.productID, 404);
          }
       })
-   });
 });
 
+
+//CREATE
 app.post('/', (req, res) => {
 
+   //Create array of new row data
    var item = req.body;
    var item = [
       item["name"],
@@ -66,63 +73,87 @@ app.post('/', (req, res) => {
       item["image"]
    ];
 
+   //Check if all fields are filled
    for (cell of item) {
       if (cell === undefined) {
          sendRequest(req, res, "At least one field is undefined", 400);
       }
    }
 
+   //Run query
    if (!req.headerSend) {
       db.all('INSERT INTO products (name, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)',
-      item, function(err, result) {
-         sendRequest(req, res);
+      item, function(err) {
+         if (err) {sendRequest(req, res, err, 500)}
+         else {sendRequest(req, res)}
       })
    }
 });
 
+
+//UPDATE
 app.put('/:productID', (req, res) => {
    var updateList = "";
-
    object = req.body;
 
+   //Compose data values to change into sql query
    for (column in object) {
       updateList += column + ' = "' + object[column] + '", ';
    }
 
+   //Trim and compose full query
    updateList = updateList.substr(0, updateList.length - 2);
-
    var query = 'UPDATE products SET ' + updateList + ' WHERE id = ' + req.params.productID;
 
-   db.run(query, function(err,result) {
-      if (this.changes !== 1) {
+   //Run query
+   db.run(query, function(err) {
+      if (err) {sendRequest(req, res, err, 500)}
+      else if (this.changes !== 1) {
          sendRequest(req, res, "No item in our database has ID: " + req.params.productID, 404);
       }
-      sendRequest(req, res);
+      else {
+         sendRequest(req, res);
+      }
    });
 });
 
+
+//DELETE
 app.delete('/:productID', (req, res) => {
-   db.run('DELETE FROM products WHERE id = ' + req.params.productID, function(err, result) {
-      if (this.changes !== 1) {
+   db.run('DELETE FROM products WHERE id = ' + req.params.productID, function(err) {
+      if (err) {sendRequest(req, res, err, 500)}
+      else if (this.changes !== 1) {
          sendRequest(req, res, "No item in our database has ID: " + req.params.productID, 404);
       }
-      sendRequest(req, res);
+      else {
+         sendRequest(req, res);
+      }
    })
-   logRequest(req, res);
 });
 
+
+//DEFAULT for unsupported requests
 app.all('*', (req, res) => {
    sendRequest(req, res, "This type of request is not supported", 403);
 })
 
+//Sends request and logs request
 sendRequest = function (req, res, message, status) {
    res.status(200);
+   //Composing appropiate return message
    if (status) {
       res.status(status);
    }
+   if (res.statusCode === 500) {
+      console.log(message);
+      message = "Internal server error";
+   }
+   if (res.statusCode !== 200) {
+      message = '{"error":"' + message + '"}';
+   }
    res.send(message);
 
-
+   //Creating log message
    var log = "";
    log += "IP: " + req.connection.remoteAddress + " ";
    log += "Method: " + req.method + " ";
